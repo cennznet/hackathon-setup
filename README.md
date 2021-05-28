0437e65afba3e1ecb0184eb485b4122f1bc3a24a
holygits <beauchjord@gmail.com>
2021-05-28 13:03:37 +1200

    Final touches

--- README.md
+++ README.md
@@ -10,6 +10,19 @@ Example code to connect an:
 2) `yarn`
 
 ## Run it
+Supply your message, path to keypair.json and the password
 ```bash
-node src/index.js "hello CENNZnet!" ./myKeyPair.json
+node src/index.js "hello CENNZnet!" ./myKeyPair.json  password
 ```
+
+output 
+```
+My CENNZnet address is: 5FWEHQqYMN8YCg8yJxKHnon7Dtx4Psp2xnjvKfQqGC6kUwgv
+Connecting to CENNZnet...
+Saying hello...
+
+Said hello 🚀: 'hello world'
+Tx hash: 0x0390f6ea97a38ef762db8c44b4e69c5d7ebf9fed9dd726ce6007162f2d3acef7
+```
+
+Got to UNcover and view the transaction!
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Api } = require('@cennznet/api');
-const { Keyring } = require('@polkadot/keyring');
+const { Keyring, KeyringPair } = require('@polkadot/keyring');
+const fs = require('fs');
+const { exit } = require('process');
 
 // A websocket address for some CENNZnet full nodes
 const NIKAU_WS = 'wss://nikau.centrality.me/public/ws';
@@ -7,27 +9,47 @@ const NIKAU_WS = 'wss://nikau.centrality.me/public/ws';
 // Send some transaction to say hello to a CENNZnet blockchain
 // message: the thing you want to say
 // keyPairPath: the file path to your account.json
-async function sayHello(message, keyPairPath) {
+// password: password for the keypair file
+async function sayHello(message, keyPairPath, password) {
     // Setup a keyring to sign messages
     let keyring = new Keyring({ type: 'sr25519' });
-    let myKeyPair = keyring.addFromJson(keyPairPath);
+    let keyPairJson = JSON.parse(fs.readFileSync(keyPairPath));
+    let myKeyPair = keyring.addFromJson(keyPairJson);
+    myKeyPair.decodePkcs8(password); // unlock the keypair
+
     console.log(`My CENNZnet address is: ${keyring.encodeAddress(myKeyPair.address)}`);
 
     // Connect to Nikau network full nodes
-    let api = await Api.create(NIKAU_WS);
-
-    // Blockchains like the message in bytes form
-    let encodedMessage = new TextEncoder().encode(message);
+    let api = await Api.create({ provider: NIKAU_WS });
+    console.log(`Connecting to CENNZnet...`);
 
     // Say hello, signing the message without keypair
-    await api.tx.system.remark(encodedMessage).signAndSend(myKeyPair);
+    console.log(`Saying hello...\n\n`);
+    let txHash = (await api.tx.system.remark(message).signAndSend(myKeyPair));
 
-    return message;
+    return [message, txHash];
 }
 
-const [message, keyPairPath] = process.argv.slice(2);
-let message = "Hello CENNZnet 🌴!";
+const [message, keyPairPath, password] = process.argv.slice(2);
+if(!message) {
+    console.error('please provide a message');
+    exit(1);
+}
+if(!keyPairPath) {
+    console.error('please provide the key pair path');
+    exit(1);
+}
+if(!password) {
+    console.error('please provide a password');
+    exit(1);
+}
 
-sayHello(message, keyPairPath)
-    .then((message) => console.log(`said hello 🚀: ${message}`))
-    .catch((err) => console.log(`couldn\'t say hello because: ${err}`));
+sayHello(message, keyPairPath, password)
+    .then(([message, txHash]) => {
+        console.log(`Said hello 🚀: '${message}'\nTx Hash: ${txHash}`)
+        exit(0);
+    })
+    .catch((err) => {
+        console.log(`Couldn\'t say hello because: ${err}`)
+        exit(1)
+    });
